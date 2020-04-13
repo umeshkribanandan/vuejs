e83d32fe93d96491454d0467c86dcc9b78d8cc4c
Umesh Kribanandan <umesh.kribanandan@gmail.com>
2020-04-14 00:14:01 +0530

    Conditional rendering by v-if and v-show

---
+++ Conditional Rendering/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "All about Socks",
+        image: "./images/vmSocks-green-onWhite.jpg",
+        link: "https://www.vuemastery.com/",
+        inventory: 110,
+        onSale: true
+    }
+})
