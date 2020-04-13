0405f9b1696dfc3b603847fc1a80aae64f2ae2a2
Umesh Kribanandan <umesh.kribanandan@gmail.com>
2020-04-14 00:03:59 +0530

    Binding Attributes in Vue

---
+++ Atrribute Binding/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "All about Socks",
+        image: "./images/vmSocks-green-onWhite.jpg",
+        link: "https://www.vuemastery.com/"
+    }
+})
